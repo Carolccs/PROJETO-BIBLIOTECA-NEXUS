c6f3d6e1549aab009045662310e37f029b2be619
Caroline Costa <costa.caroline@academico.ifpb.edu.br>
2026-05-07 14:56:48 -0300

    edicao main retirada import desnecessario

--- src/main/java/br/edu/ifpb/poo/aplicacao/Main.java
+++ src/main/java/br/edu/ifpb/poo/aplicacao/Main.java
@@ -2,8 +2,6 @@ package br.edu.ifpb.poo.aplicacao;
 
 import java.util.Locale;
 
-import br.edu.ifpb.poo.servico.BibliotecaServico;
-
 public class Main {
 
     public static void main(String[] args) {
